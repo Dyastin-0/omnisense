772fb39dbbda3aa1bf4f76ad5806faa36cc436f9
Justine Paralejas <paralejasjustine15@gmail.com>
2024-12-09 15:48:15 +0800

    Build optimization.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,18 +3,21 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
   build: {
-    sourcemap: false,
-    minify: "esbuild",
-  },
-  server: {
-    hmr: {
-      overlay: false,
+    target: "esnext", // Modern JavaScript for smaller builds
+    sourcemap: false, // Disable sourcemaps to reduce build time
+    chunkSizeWarningLimit: 1000, // Increase limit for chunk size warnings
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("firebase")) {
+              return "firebase"; // Separate Firebase into its own chunk
+            }
+            return "vendor"; // Other vendor libraries
+          }
+        },
+      },
     },
   },
 });
